d0a3a1071f7a6c6e68b6c7b6a4ddc797c1db9aa3
zhichazhang <zhichazhang@tencent.com>
2020-04-19 18:23:35 +0800

    feat: 问题demo

--- config/config.ts
+++ config/config.ts
@@ -1,105 +1,57 @@
-// https://umijs.org/config/
-import { defineConfig } from 'umi';
-import defaultSettings from './defaultSettings';
-import proxy from './proxy';
+import { defineConfig, utils } from 'umi';
 
-const { REACT_APP_ENV } = process.env;
+const { winPath } = utils;
+
+import routes from './routes';
 
 export default defineConfig({
-  hash: true,
+  // 通过 package.json 自动挂载 umi 插件，不需再次挂载
+  // plugins: [],
   antd: {},
   dva: {
     hmr: true,
   },
+  // routes,
   locale: {
-    // default zh-CN
     default: 'zh-CN',
-    // default true, when it is true, will use `navigator.language` overwrite default
-    antd: true,
     baseNavigator: true,
   },
   dynamicImport: {
+    // 无需 level, webpackChunkName 配置
+    // loadingComponent: './components/PageLoading/index'
     loading: '@/components/PageLoading/index',
   },
-  targets: {
-    ie: 11,
-  },
-  // umi routes: https://umijs.org/docs/routing
-  routes: [
-    {
-      path: '/user',
-      component: '../layouts/UserLayout',
-      routes: [
-        {
-          name: 'login',
-          path: '/user/login',
-          component: './user/login',
+  // 暂时关闭
+  pwa: false,
+  lessLoader: { javascriptEnabled: true },
+  cssLoader: {
+    // 这里的 modules 可以接受 getLocalIdent
+    modules: {
+      getLocalIdent: (
+        context: {
+          resourcePath: string;
         },
-      ],
+        _: string,
+        localName: string,
+      ) => {
+        if (
+          context.resourcePath.includes('node_modules') ||
+          context.resourcePath.includes('ant.design.pro.less') ||
+          context.resourcePath.includes('global.less')
+        ) {
+          return localName;
+        }
+        const match = context.resourcePath.match(/src(.*)/);
+        if (match && match[1]) {
+          const antdProPath = match[1].replace('.less', '');
+          const arr = winPath(antdProPath)
+            .split('/')
+            .map((a: string) => a.replace(/([A-Z])/g, '-$1'))
+            .map((a: string) => a.toLowerCase());
+          return `antd-pro${arr.join('-')}-${localName}`.replace(/--/g, '-');
+        }
+        return localName;
+      },
     },
-    {
-      path: '/',
-      component: '../layouts/SecurityLayout',
-      routes: [
-        {
-          path: '/',
-          component: '../layouts/BasicLayout',
-          authority: ['admin', 'user'],
-          routes: [
-            {
-              path: '/',
-              redirect: '/welcome',
-            },
-            {
-              path: '/welcome',
-              name: 'welcome',
-              icon: 'smile',
-              component: './Welcome',
-            },
-            {
-              path: '/admin',
-              name: 'admin',
-              icon: 'crown',
-              component: './Admin',
-              authority: ['admin'],
-              routes: [
-                {
-                  path: '/admin/sub-page',
-                  name: 'sub-page',
-                  icon: 'smile',
-                  component: './Welcome',
-                  authority: ['admin'],
-                },
-              ],
-            },
-            {
-              name: 'list.table-list',
-              icon: 'table',
-              path: '/list',
-              component: './ListTableList',
-            },
-            {
-              component: './404',
-            },
-          ],
-        },
-        {
-          component: './404',
-        },
-      ],
-    },
-    {
-      component: './404',
-    },
-  ],
-  // Theme for antd: https://ant.design/docs/react/customize-theme-cn
-  theme: {
-    // ...darkTheme,
-    'primary-color': defaultSettings.primaryColor,
-  },
-  ignoreMomentLocale: true,
-  proxy: proxy[REACT_APP_ENV || 'dev'],
-  manifest: {
-    basePath: '/',
   },
 });
